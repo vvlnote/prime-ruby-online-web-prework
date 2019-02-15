115eff47c47e3192e3423d2384b37c5f0b53f073
Joy Lin <vvlnote@hotmail.com>
2019-02-15 01:55:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num < 0
+    return false
+  else
+    for divider in 2...num do
+      if num % divider == 0
+        return false
+      end
+    end
+  end
+  return true
+end
